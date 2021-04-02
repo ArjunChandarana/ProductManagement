96f93b2972509d1eb7197b0ec7e152fc11c0857e
Arjun Chandarana <achandarana801@gmail.com>
2021-04-02 18:32:11 +0530

    Updated

--- ProductManagement/Controllers/ProductsController.cs
+++ ProductManagement/Controllers/ProductsController.cs
@@ -16,52 +16,66 @@ namespace ProductManagement.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
      
-
+        public ActionResult Error()
+        {
+            return View();
+        }
         // GET: Products
         public ActionResult Index(string Sorting_Order, string Search_Data, string Filter_Value, int? Page_No)
         {
-            /*var products = db.Products.Include(p => p.Category);
-            return View(products.ToList());*/
-
-            var products = db.Products.Include(p => p.Category);
-            if (Search_Data != null)
+            try
             {
-                Page_No = 1;
-            }
-            else
-            {
-                Search_Data = Filter_Value;
-            }
+                Log.Info("Product Index Visible");
+                /*var products = db.Products.Include(p => p.Category);
+                return View(products.ToList());*/
 
-            ViewBag.FilterValue = Search_Data;
-            if (!String.IsNullOrEmpty(Search_Data))
+                var products = db.Products.Include(p => p.Category);
+                if (Search_Data != null)
+                {
+                    Page_No = 1;
+                }
+                else
+                {
+                    Search_Data = Filter_Value;
+                }
 
-            {
-                products = products.Where(p => p.Name.ToUpper().Contains(Search_Data.ToUpper())
-                    || p.Category.CategoryName.ToUpper().Contains(Search_Data.ToUpper()));
+                ViewBag.FilterValue = Search_Data;
+                if (!String.IsNullOrEmpty(Search_Data))
+
+                {
+                    products = products.Where(p => p.Name.ToUpper().Contains(Search_Data.ToUpper())
+                        || p.Category.CategoryName.ToUpper().Contains(Search_Data.ToUpper()));
+                }
+                ViewBag.CurrentSortOrder = Sorting_Order;
+                ViewBag.SortingName = String.IsNullOrEmpty(Sorting_Order) ? "Name" : "";
+                ViewBag.SortingDate = Sorting_Order == "Date_Enroll" ? "Category_dec" : "Category";
+
+                switch (Sorting_Order)
+                {
+                    case "Name":
+                        products = products.OrderByDescending(p => p.Name);
+                        break;
+                    case "Category":
+                        products = products.OrderBy(p => p.Category.CategoryName);
+                        break;
+                    case "Category_dec":
+                        products = products.OrderByDescending(p => p.Category.CategoryName);
+                        break;
+                    default:
+                        products = products.OrderBy(p => p.Name);
+                        break;
+                }
+                int Size_Of_Page = 4;
+                int No_Of_Page = (Page_No ?? 1);
+                return View(products.ToPagedList(No_Of_Page, Size_Of_Page));
             }
-            ViewBag.CurrentSortOrder = Sorting_Order;
-            ViewBag.SortingName = String.IsNullOrEmpty(Sorting_Order) ? "Name" : "";
-            ViewBag.SortingDate = Sorting_Order == "Date_Enroll" ? "Category_dec" : "Category";
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                return RedirectToAction("Error","Products");
+
+            }
            
-            switch (Sorting_Order)
-            {
-                case "Name":
-                    products = products.OrderByDescending(p => p.Name);
-                    break;
-                case "Category":
-                    products = products.OrderBy(p => p.Category.CategoryName);
-                    break;
-                case "Category_dec":
-                    products = products.OrderByDescending(p => p.Category.CategoryName);
-                    break;
-                default:
-                    products = products.OrderBy(p => p.Name);
-                    break;
-            }
-            int Size_Of_Page = 4;
-            int No_Of_Page = (Page_No ?? 1);
-            return View(products.ToPagedList(No_Of_Page, Size_Of_Page));
 
             
 
@@ -72,15 +86,26 @@ namespace ProductManagement.Controllers
         [Authorize]
         public ActionResult MultipleDelete(int[] ids)
         {
-            foreach (int id in ids)
+            try
             {
-                var product = this.db.Products.Find(id);
-                this.db.Products.Remove(product);
-                this.db.SaveChanges();
+                foreach (int id in ids)
+                {
+                    var product = this.db.Products.Find(id);
+                    this.db.Products.Remove(product);
+                    this.db.SaveChanges();
+                }
+                TempData["Type"] = 0;
+                TempData["Message"] = "Products Deleted Successfully";
+                throw new Exception();
+                return RedirectToAction("Index");
             }
-            TempData["Type"] = 0;
-            TempData["Message"] = "Products Deleted Successfully";
-            return RedirectToAction("Index");
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                return RedirectToAction("Error", "Products");
+
+            }
+
         }
 
         // GET: Products/Details/5
@@ -114,9 +139,11 @@ namespace ProductManagement.Controllers
         [Authorize]
         public ActionResult Create(Product product)
         {
-            if (ModelState.IsValid)
+            try
             {
-                
+                if (ModelState.IsValid)
+                {
+
                     if (product.SImage.ContentLength > 2 * 1024 * 1024)
                     {
                         ModelState.AddModelError("SImage", "File size cannot be more then 2Mb");
@@ -128,7 +155,7 @@ namespace ProductManagement.Controllers
                         fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
                         product.Smallimage = "~/Image/" + fileName;
                         fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
-                        product.SImage.SaveAs(fileName);                        
+                        product.SImage.SaveAs(fileName);
                     }
                     if (product.LImage.ContentLength > 5 * 1024 * 1024)
                     {
@@ -145,33 +172,50 @@ namespace ProductManagement.Controllers
                     }
 
 
-                db.Products.Add(product);
-                db.SaveChanges();
-                TempData["Type"] = 0;
-                TempData["Message"] = "Product Added Successfully";
-                return RedirectToAction("Index");
-            }
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    TempData["Type"] = 0;
+                    TempData["Message"] = "Product Added Successfully";
+                    return RedirectToAction("Index");
+                }
 
-            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
-            return View(product);
+                ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
+                return View(product);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                return RedirectToAction("Error", "Products");
+
+            }
+            
         }
 
         // GET: Products/Edit/5
         public ActionResult Edit(int? id)
         {
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Product product = db.Products.Find(id);
+                if (product == null)
+                {
+                    return HttpNotFound();
+                }
+                ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
+                TempData["Type"] = 0;
+                TempData["Message"] = "Product edited Successfully";
+                return View(product);
             }
-            Product product = db.Products.Find(id);
-            if (product == null)
+            catch (Exception ex)
             {
-                return HttpNotFound();
+                Log.Error(ex.Message);
+                return RedirectToAction("Error", "Products");
+
             }
-            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
-            TempData["Type"] = 0;
-            TempData["Message"] = "Product edited Successfully";
-            return View(product);
         }
 
         // POST: Products/Edit/5
@@ -182,60 +226,69 @@ namespace ProductManagement.Controllers
         [Authorize]
         public ActionResult Edit(Product product)
         {
-            if (ModelState.IsValid)
+            try
             {
-                if (product.SImage != null)
+                if (ModelState.IsValid)
                 {
-                    if (product.SImage.ContentLength > 2 * 1024 * 1024)
+                    if (product.SImage != null)
                     {
-                        ModelState.AddModelError("SImage", "File size cannot be more then 2Mb");
-                    }
-                    else
-                    {
-                        if (System.IO.File.Exists(Server.MapPath(product.Smallimage)))
+                        if (product.SImage.ContentLength > 2 * 1024 * 1024)
                         {
-                            System.IO.File.Delete(Server.MapPath(product.Smallimage));
-
+                            ModelState.AddModelError("SImage", "File size cannot be more then 2Mb");
                         }
-                        string fileName = Path.GetFileNameWithoutExtension(product.SImage.FileName);
-                        string extension = Path.GetExtension(product.SImage.FileName).ToLower();
-                        fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
-                        product.Smallimage = "~/Image/" + fileName;
-                        fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
-                        product.SImage.SaveAs(fileName);
-                    }
-                }
-                if (product.LImage != null)
-                {
-                    if (product.LImage.ContentLength > 5 * 1024 * 1024)
-                    {
-                        ModelState.AddModelError("SImage", "File size cannot be more then 5Mb");
-                    }
-                    else
-                    {
-                        if (System.IO.File.Exists(Server.MapPath(product.Largeimage)))
+                        else
                         {
-                            System.IO.File.Delete(Server.MapPath(product.Largeimage));
+                            if (System.IO.File.Exists(Server.MapPath(product.Smallimage)))
+                            {
+                                System.IO.File.Delete(Server.MapPath(product.Smallimage));
 
+                            }
+                            string fileName = Path.GetFileNameWithoutExtension(product.SImage.FileName);
+                            string extension = Path.GetExtension(product.SImage.FileName).ToLower();
+                            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                            product.Smallimage = "~/Image/" + fileName;
+                            fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+                            product.SImage.SaveAs(fileName);
                         }
-                        string fileName = Path.GetFileNameWithoutExtension(product.LImage.FileName);
-                        string extension = Path.GetExtension(product.LImage.FileName).ToLower();
-                        fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
-                        product.Largeimage = "~/Image/" + fileName;
-                        fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
-                        product.LImage.SaveAs(fileName);
                     }
+                    if (product.LImage != null)
+                    {
+                        if (product.LImage.ContentLength > 5 * 1024 * 1024)
+                        {
+                            ModelState.AddModelError("SImage", "File size cannot be more then 5Mb");
+                        }
+                        else
+                        {
+                            if (System.IO.File.Exists(Server.MapPath(product.Largeimage)))
+                            {
+                                System.IO.File.Delete(Server.MapPath(product.Largeimage));
+
+                            }
+                            string fileName = Path.GetFileNameWithoutExtension(product.LImage.FileName);
+                            string extension = Path.GetExtension(product.LImage.FileName).ToLower();
+                            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                            product.Largeimage = "~/Image/" + fileName;
+                            fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+                            product.LImage.SaveAs(fileName);
+                        }
+                    }
+
+
+                    db.Entry(product).State = EntityState.Modified;
+                    db.SaveChanges();
+                    TempData["Type"] = 0;
+                    TempData["Message"] = "Product edited Successfully";
+                    return RedirectToAction("Index");
                 }
-
-
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                TempData["Type"] = 0;
-                TempData["Message"] = "Product edited Successfully";
-                return RedirectToAction("Index");
+                ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
+                return View(product);
             }
-            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
-            return View(product);
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                return RedirectToAction("Error", "Products");
+
+            }
         }
 
         // GET: Products/Delete/5
